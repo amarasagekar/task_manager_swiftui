f25a4b377ca9f104c4878d0ab17dab51777ac418
Amar Asagekar <amarasagekar@gmail.com>
2024-01-21 20:16:43 +0530

    Completed app functionality

---
+++ TaskManager/TaskManager/TasksView.swift
@@ -0,0 +1,18 @@
+//
+//  TasksView.swift
+//  TaskManager
+//
+//  Created by AMAR on 21/01/24.
+//
+
+import SwiftUI
+
+struct TasksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TasksView()
+}
